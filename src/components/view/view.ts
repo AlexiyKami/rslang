f54f5c43a490e55aaa0cb30ed5061cdcd787feb5
Антон Щерба <Tony.Shcherba@gmail.com>
2022-08-17 00:35:33 +0300

    feat: implement navigation call

--- src/components/view/view.ts
+++ src/components/view/view.ts
@@ -1,10 +1,12 @@
 import Controller from '../controller/controller';
+import Navigation from './navigation/navigation';
 
 class View {
   private controller: Controller;
 
   constructor(controller: Controller) {
     this.controller = controller;
+    new Navigation();
   }
 }
 

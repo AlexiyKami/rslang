d3e74b39591f1a5b652d545f4315e11d39980d17
Aliseiko <aliseiko.dev@gmail.com>
2022-08-19 20:17:16 +0300

    feat: apdate resetState params

--- src/components/app/audioChallengeModel/audioChallengeModel.ts
+++ src/components/app/audioChallengeModel/audioChallengeModel.ts
@@ -3,18 +3,27 @@ import { Word } from '../../types/types';
 import View from '../../view/view';
 
 class AudioChallengeModel {
-  private state: { currentWords: Word[]; currentWordNumber: number };
+  private state: {
+    currentWords: Word[];
+    currentWordNumber: number;
+    rightWords: string[];
+    wrongWords: string[];
+  };
 
   constructor(private readonly view: View, private readonly model: AppModel) {
     this.state = {
       currentWords: [],
       currentWordNumber: 0,
+      rightWords: [],
+      wrongWords: [],
     };
   }
 
   private resetState(): void {
     this.state.currentWordNumber = 0;
-    this.state.currentWords = [];
+    this.state.currentWords.length = 0;
+    this.state.rightWords.length = 0;
+    this.state.wrongWords.length = 0;
   }
 
   onWordsLoadError(errorMessage: string) {

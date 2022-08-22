6120e54f1875ed36ba42cdfe8032ec8db72dc0d3
Aliseiko <aliseiko.dev@gmail.com>
2022-08-22 16:18:59 +0300

    feat: add minigames page

---
+++ src/components/view/minigamesPage/minigamesPageView.ts
@@ -0,0 +1,43 @@
+import './minigamesPage.scss';
+import { getElement } from '../../utils/utils';
+import './../../../assets/images/sprint.png';
+import './../../../assets/images/audioChallenge.png';
+import Controller from '../../controller/controller';
+
+class MinigamesPageView {
+  private mainWindow: HTMLElement;
+
+  constructor(private readonly controller: Controller) {
+    this.mainWindow = getElement('main-window') as HTMLElement;
+  }
+  public renderMinigamesPage() {
+    this.mainWindow.innerHTML = `
+    <div class="minigames-page">
+      <div class="minigames-page__games-block">
+        <div class="minigames-page__game-card">
+          <img class="minigames-page__game-card-img" src="./../../../assets/images/sprint.png"
+               alt="Audio Challenge game">
+          <h2 class="minigames-page__game-card-title">Sprint</h2>
+          <p class="minigames-page__game-card-text">Check how much points you can get in one minute, making educated
+            guesses about what is right and what is wrong.</p>
+          <button class="minigames-page__game-card-button minigames-page__game-card-button_sprint button" type="button">Play</button>
+        </div>
+        <div class="minigames-page__game-card">
+          <img class="minigames-page__game-card-img" src="./../../../assets/images/audioChallenge.png"
+               alt="Audio Challenge game">
+          <h2 class="minigames-page__game-card-title">Audio challenge</h2>
+          <p class="minigames-page__game-card-text">Check your listening skills, trying to pick the right meaning after
+            hearing a word. Be careful, as you just have one guess.</p>
+          <button class="minigames-page__game-card-button minigames-page__game-card-button_audiochallenge button" type="button">Play</button>
+        </div>
+      </div>
+    </div>
+    `;
+
+    (getElement('minigames-page__game-card-button_audiochallenge') as HTMLButtonElement).addEventListener('click', () =>
+      this.controller.audioChallengeController.initAudioChallengeGame()
+    );
+  }
+}
+
+export default MinigamesPageView;
--- src/components/view/view.ts
+++ src/components/view/view.ts
@@ -2,21 +2,26 @@ import Controller from '../controller/controller';
 import Dictionary from './dictionary/dictionary';
 import Navigation from './navigation/navigation';
 import AudioChallengeView from './audioChallenge/audioChallengeView';
+import MinigamesPageView from './minigamesPage/minigamesPageView';
+import { getElement } from '../utils/utils';
 
 class View {
   private dictionary: Dictionary;
   audioChallenge: AudioChallengeView;
   private navigation: Navigation;
+  private MinigamesPage: MinigamesPageView;
 
   constructor(private readonly controller: Controller) {
     this.navigation = new Navigation();
     this.dictionary = new Dictionary(this.controller);
     this.audioChallenge = new AudioChallengeView(this.controller, this);
+    this.MinigamesPage = new MinigamesPageView(this.controller);
   }
 
   public initRender() {
-    this.dictionary.draw();
+    // this.dictionary.draw();
     // this.controller.audioChallengeController.initAudioChallengeGame();
+    this.MinigamesPage.renderMinigamesPage();
   }
 }
 

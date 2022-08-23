8cd3566e2f2930308a26011bba0f35d8db983462
Aliseiko <aliseiko.dev@gmail.com>
2022-08-23 09:21:49 +0300

    feat: add method to save app state to localstorage

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,20 +1,27 @@
 import Controller from '../controller/controller';
 import View from '../view/view';
 import AudioChallengeModel from './audioChallengeModel/audioChallengeModel';
+import { AppState } from '../types/types';
 
 class AppModel {
   public controller: Controller;
   public view: View;
   public audioChallengeModel: AudioChallengeModel;
+  public state: AppState;
 
   constructor() {
     this.controller = new Controller(this);
     this.view = new View(this.controller);
     this.audioChallengeModel = new AudioChallengeModel(this.view, this);
+    this.state = JSON.parse(localStorage.getItem('RSSLang-state') || '{}');
   }
 
-  initApp() {
+  initApp(): void {
     this.view.initRender();
+  }
+
+  saveState(): void {
+    localStorage.setItem('RSSLang-state', JSON.stringify(this.state));
   }
 }
 

e569e5ecfe81b8a9593e1d08b33906e3b7de55b3
Alex <AlexiyKami@github.com>
2022-08-27 04:18:05 +0300

    feat: add charts

--- src/components/view/statisticsPage/statisticsPage.ts
+++ src/components/view/statisticsPage/statisticsPage.ts
@@ -1,6 +1,7 @@
 import Controller from '../../controller/controller';
 import View from '../view';
 import './statisticsPage.scss';
+import Chart from 'chart.js/auto';
 
 class StatisticsPage {
   controller: Controller;
@@ -9,7 +10,8 @@ class StatisticsPage {
     this.controller = controller;
     this.view = view;
   }
-  draw(): void {
+  async draw(): Promise<void> {
+    this.controller.showLoadingPopup();
     this.view.changeAppTitle('Statistics');
     (document.querySelector('.main-window') as HTMLElement).innerHTML = `
       <div class='statistics-page'>
@@ -18,7 +20,7 @@ class StatisticsPage {
           <div class='today-indicators'>
             <div class='learned-words'>
               <h3 class='learned-words-title'>Слов изучено</h3>
-              <h3 class='learned-words-counter'>3600</h3>
+              <h3 class='learned-words-counter'>0</h3>
             </div>
             <div class='accuracy'>
               <h3 class='accuracy-title'>Точность</h3>
@@ -61,10 +63,102 @@ class StatisticsPage {
           </div>
         </div>
         <div class='alltime-statistics'>
-          
+          <h3 class='statistics-title'>Все время</h3>
+          <div class='alltime-statistics-charts'>
+            <div class='alltime-words'>
+              <canvas id='chart-words' width='400' height='300'></canvas>
+            </div>
+            <div class='alltime-progress'>
+              <canvas id='chart-progress' width='400' height='300'></canvas>
+            </div>
+          </div>
         </div>
       </div>
     `;
+
+    const wordsChart = document.getElementById('chart-words') as HTMLCanvasElement;
+    const progressChart = document.getElementById('chart-progress') as HTMLCanvasElement;
+    new Chart(wordsChart, {
+      type: 'line',
+      data: {
+        labels: ['22.08.2022', '23.08.2022', '24.08.2022', '25.08.2022', '26.08.2022', '27.08.2022'], //  labels[] and data[] has same length, labels starts with earliest Date
+        datasets: [
+          {
+            label: 'Слова',
+            data: [1, 25, 8, 2, 0, 15],
+            backgroundColor: 'orange',
+            borderColor: 'lightcoral',
+            borderWidth: 2,
+          },
+        ],
+      },
+      options: {
+        elements: {
+          line: {
+            tension: 0.3,
+          },
+        },
+        scales: {
+          xAxes: {
+            grid: {
+              display: false,
+            },
+          },
+        },
+        plugins: {
+          legend: {
+            display: true,
+            labels: {
+              font: {
+                size: 20,
+              },
+            },
+            onClick: () => {},
+          },
+        },
+      },
+    });
+    new Chart(progressChart, {
+      type: 'line',
+      data: {
+        labels: ['22.08.2022', '23.08.2022', '24.08.2022', '25.08.2022', '26.08.2022', '27.08.2022'],
+        datasets: [
+          {
+            label: 'Прогресс',
+            data: [1, 2, 5, 3, 8, 2],
+            backgroundColor: 'green',
+            borderColor: 'lightgreen',
+            borderWidth: 2,
+          },
+        ],
+      },
+      options: {
+        elements: {
+          line: {
+            tension: 0.3,
+          },
+        },
+        scales: {
+          xAxes: {
+            grid: {
+              display: false,
+            },
+          },
+        },
+        plugins: {
+          legend: {
+            display: true,
+            labels: {
+              font: {
+                size: 20,
+              },
+            },
+            onClick: () => {},
+          },
+        },
+      },
+    });
+    this.controller.hideLoadingPopup();
   }
 }
 

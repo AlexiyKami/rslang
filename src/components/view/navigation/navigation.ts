6e2c1d2ff88e973aa1d110b855e44a6a72b2168d
Антон Щерба <Tony.Shcherba@gmail.com>
2022-08-31 11:33:31 +0300

    feat: add dict nav btn

--- src/components/view/navigation/navigation.ts
+++ src/components/view/navigation/navigation.ts
@@ -7,6 +7,7 @@ export default class Navigation {
   private readonly radioIdLabelMap = new Map<string, string>([
     ['radio-home', 'Home'],
     ['radio-book', 'Textbook'],
+    ['radio-bookmark', 'Dictionary'],
     ['radio-games', 'Minigames'],
     ['radio-stat', 'Statistics'],
   ]);
@@ -16,8 +17,16 @@ export default class Navigation {
   constructor(private readonly controller: Controller, private readonly view: View) {
     this.navEl = document.createElement('nav');
     this.navEl.classList.add('page-nav');
+    this.navEl.innerHTML = `
+    <input id="menu-toggle" type="checkbox">
+    <label class='menu-button-container' for="menu-toggle">
+      <div class='menu-button'></div>
+    </label>
+    `;
 
     for (const entry of this.radioIdLabelMap) {
+      if (entry[1] === 'Dictionary' && !controller.isAuthorized()) continue;
+
       const radioEl = document.createElement('input');
       radioEl.type = 'radio';
       radioEl.name = 'navigation';
@@ -33,7 +42,13 @@ export default class Navigation {
       if (entry[1] === 'Textbook')
         radioLabelEl.addEventListener('click', () => {
           this.controller.playStopAudio('', false);
-          this.view.dictionary.draw();
+          if (this.controller.dictionary.getDictionaryGroup() !== 6) this.view.dictionary.draw();
+          else this.controller.dictionary.setDictionaryGroup(0);
+        });
+      else if (entry[1] === 'Dictionary')
+        radioLabelEl.addEventListener('click', () => {
+          this.controller.playStopAudio('', false);
+          this.controller.dictionary.setDictionaryGroup(6);
         });
       else if (entry[1] === 'Minigames')
         radioLabelEl.addEventListener('click', () => {
@@ -59,8 +74,15 @@ export default class Navigation {
     } else if (this.controller.navController.curPage === 1) {
       this.view.dictionary.draw();
     } else if (this.controller.navController.curPage === 2) {
-      this.view.MinigamesPage.renderMinigamesPage();
+      if (this.controller.isAuthorized()) {
+        this.controller.dictionary.setDictionaryGroup(6);
+      } else {
+        this.controller.dictionary.setDictionaryGroup(1);
+        this.controller.navController.curPage = 1;
+      }
     } else if (this.controller.navController.curPage === 3) {
+      this.view.MinigamesPage.renderMinigamesPage();
+    } else if (this.controller.navController.curPage === 4) {
       this.view.statisticsPage.draw();
     }
 
@@ -71,6 +93,13 @@ export default class Navigation {
     });
 
     document.getElementsByTagName('body')[0].prepend(this.navEl);
+
+    const toggle = document.getElementById('menu-toggle') as HTMLElement;
+    toggle.addEventListener('change', (event: Event) => {
+      const target = event.target as HTMLInputElement;
+      if (target.checked) this.navEl.classList.add('extend');
+      else this.navEl.classList.remove('extend');
+    });
   }
 
   public hideNav() {
